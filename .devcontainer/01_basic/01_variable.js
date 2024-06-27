fe2a532870e93921470484fae8c7c9414988a48b
Ronit Roy <98654461+ronitroy1309@users.noreply.github.com>
2024-06-27 16:41:40 +0000

    CONTAINER CONFIG DONE

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,6 @@
+const accountId = 143255
+let accountemail = "rony@gmail.com"
+let accountpassword = "23415"
+let accountCity = "banglore"
+
+console.table([accountId,accountemail,accountpassword,accountCity])
