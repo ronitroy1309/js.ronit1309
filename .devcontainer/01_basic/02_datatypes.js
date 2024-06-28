fabdc2e946e14b3a492afa9e2fe3f8ae855faf61
Ronit Roy <98654461+ronitroy1309@users.noreply.github.com>
2024-06-28 04:49:32 +0000

    config done

---
+++ .devcontainer/01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict"//treats all js code as newer version
+//alert(3+3)//we are using nodejs not browser
+
+//NUMBER=>2 to power 53
+//bollean => yes no
+//bigInt
+//string=>""
+//null = standaloine value
+//undeined
+
+console.log(typeof undefined  );
