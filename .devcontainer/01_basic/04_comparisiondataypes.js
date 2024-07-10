bd02f446834774fba71436394e6a02db8ee3b295
Ronit Roy <98654461+ronitroy1309@users.noreply.github.com>
2024-07-10 04:42:34 +0000

    config done

---
+++ .devcontainer/01_basic/04_comparisiondataypes.js
@@ -0,0 +1,3 @@
+/*
+comparison converts nul to a number hence treaating as a 0that's why (3)null >=0 and (1) null > 0 iss false5
+*/
---
+++ .devcontainer/01_basic/datatype_summarty.js
@@ -0,0 +1,13 @@
+/*primitive
+7 type :string,number,boolean,null,undefined,symbol,BigInt(big values,scientific value store)
+
+*/
+
+/* javascript k andar hm language define nhi krte*/
+
+/*
+Reference(Non Primitive) : Vo values jinki memory mai reference alloct kiya ja skta hai
+ Array,Objects,functions,
+*/
+
+ 
